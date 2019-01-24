4685e8c5b62f2ed70dbddcf6de05639f44adf023
Pamela Frieze <pfrieze@gmail.com>
2019-01-24 16:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,8 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   case 'generous':
     return 'Thank you so much.';
-  break;
   case 'not as generous':
     return 'Thank you.';
-  break;
    default:
     return 'Bye.';
   break;
