dfef2d070e0b09f67cb7ea4d34445fc10f6aeaa8
Pamela Frieze <pfrieze@gmail.com>
2019-01-24 16:34:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet)
-  if (ride <= 400) { 
+  if (feet <= 400) { 
   scuberGreetingForFeet = 'This one is on me!';
     
   } else if (ride > 400){
